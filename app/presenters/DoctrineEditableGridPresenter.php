658d077514b7dc2eca7fb2ac2020400b93db70f8
Pavel Máca <maca.pavel@gmail.com>
2011-01-23 08:54:35 +0100

    update DoctrineEditableGrid example

--- app/presenters/DoctrineEditableGridPresenter.php
+++ app/presenters/DoctrineEditableGridPresenter.php
@@ -8,55 +8,59 @@ use Nette\Forms\Form;
  * @author Pavel Máca
  * @license MIT
  */
-class DoctrineEditableGridPresenter extends BasePresenter
-{
-	protected function createComponentGrid($name)
-	{
+class DoctrineEditableGridPresenter extends BasePresenter {
+
+	protected function createComponentGrid($name) {
 		$grid = new Gridito\EditableGrid($this, $name);
 
 		// model
 		$em = Nette\Environment::getService("Doctrine\ORM\EntityManager");
-		$model = new Model\UsersGriditoDoctrineModel($em);
-		$grid->setEditableModel($model);
+		$model = new Model\UsersEditableGriditoDoctrineModel($em);
+		$grid->setModel($model);
 
 		// columns
 		$grid->addColumn("id", "ID")
 			->setSortable(true);
-		
+
 		$grid->addColumn("username", "Username")
 			->setSortable(true)
 			->setEditable();
-		
+
 		$grid->addColumn("name", "Name")
 			->setSortable(true)
 			->setEditable();
-		
+
 		$grid->addColumn("surname", "Surname")
 			->setSortable(true)
 			->setEditable();
-		
+
 		$grid->addColumn("mail", "E-mail", array(
 			"sortable" => true,
 			"renderer" => function ($row) {
 				echo Nette\Web\Html::el("a")->href("mailto:$row->mail")->setText($row->mail);
-			}
-			//TODO editable dosn't work here
+			},
+			"editable" => true,
 		));
-		
-		$grid->getColumn("mail")
-			->setEditable()
-				->addRule(Form::EMAIL, "E-mail is not valid.");
-	
+
 			
+		$grid->getColumn("mail")
+			->getEditable()
+			->addRule(Form::EMAIL, "E-mail is not valid.");
+
+
 		$grid->addColumn("active", "Active")
 			->setSortable(true)
 			->setEditable('Nette\Forms\Checkbox');
 
+		$grid->getEditableForm()
+			->addSubmit("save", "Uložit");
+
 		// toolbar buttons
+		
 		$grid->addAddButton("create", "Create new user", array(
 			"icon" => "ui-icon-plusthick",
 		));
-		
+
 		$grid->addToolbarButton("back", "Go back to examples", array(
 			"link" => $this->link("Homepage:"),
 			"icon" => "ui-icon-home",
@@ -73,13 +77,26 @@ class DoctrineEditableGridPresenter extends BasePresenter
 				}
 			},
 			"visible" => function ($user) {
-				return !$user->isActive();
+				return!$user->isActive();
 			},
 		));
-			
+
 		$grid->addEditButton("edit", "Edit", array(
 			"icon" => "ui-icon-pencil",
+			"editedMessage" => "Uloženo.",
 		));
+
+
+		$grid->getModel()->setEntityUpdateHandler(function($entity, $values) use ($grid) {
+				foreach ($values as $key => $val) {
+					$method = "set" . ucfirst($key);
+					if (method_exists($entity, $method)) {
+						$entity->$method($val);
+					} else {
+						throw new \InvalidArgumentException("Entity has not method '$method'");
+					}
+				}
+			});
 	}
 
 }
